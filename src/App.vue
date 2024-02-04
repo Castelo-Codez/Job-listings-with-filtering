dc4c59024a505463a9f06d2da9c427c33e4089fc
castelo <castlogomaa459@gmail.com>
2024-02-04 11:08:42 +0200

    remove unneeded hooks (onMounted)

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup>
-import {onMounted} from "vue";
 import MainContainer from "./components/MainContainer.vue";
 import HeaderBackground from "./components/HeaderBackground.vue";
 </script>
